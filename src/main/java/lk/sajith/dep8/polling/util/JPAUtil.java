f32a4873c7bc1d30732aa18cfa06d6f1429d3f01
sajith <sajithd784@gmail.com>
2022-06-18 22:02:51 +0530

    Finalize the development of JPAUtil and test the JPAUtil

--- src/main/java/lk/sajith/dep8/polling/util/JPAUtil.java
+++ src/main/java/lk/sajith/dep8/polling/util/JPAUtil.java
@@ -2,12 +2,20 @@ package lk.sajith.dep8.polling.util;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.io.IOException;
+import java.util.Properties;
 
-public class JPAUtil {
+public abstract class  JPAUtil {
     private static EntityManagerFactory emf = buildEntityManagerFactory();
 
     private static EntityManagerFactory buildEntityManagerFactory() {
-        return Persistence.createEntityManagerFactory("poll");
+        Properties prop = new Properties();
+        try {
+            prop.load(JPAUtil.class.getResourceAsStream("/application-test.properties"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return Persistence.createEntityManagerFactory("poll", prop);
     }
 
     public static EntityManagerFactory getEntityManagerFactory(){
