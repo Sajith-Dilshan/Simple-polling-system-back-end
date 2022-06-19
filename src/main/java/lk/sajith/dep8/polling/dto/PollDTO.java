30e46d0a07c8479a52957bef5eb970e8f3f13320
sajith <sajithd784@gmail.com>
2022-06-19 17:57:59 +0530

    Add PollDTO

---
+++ src/main/java/lk/sajith/dep8/polling/dto/PollDTO.java
@@ -0,0 +1,26 @@
+package lk.sajith.dep8.polling.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PollDTO implements Serializable {
+
+    private Integer id;
+    private String title;
+    private String createdBy;
+    private int upVotes;
+    private int downVotes;
+
+    public PollDTO(String title, String createdBy, int upVotes, int downVotes) {
+        this.title = title;
+        this.createdBy = createdBy;
+        this.upVotes = upVotes;
+        this.downVotes = downVotes;
+    }
+}
