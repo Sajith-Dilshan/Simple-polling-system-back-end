a0003098c0ae6c30aea1b6ba14e0e708e811fb89
sajith <sajithd784@gmail.com>
2022-06-19 10:29:38 +0530

    Implement CrudDAOImpl

--- src/main/java/lk/sajith/dep8/polling/dao/CrudDAO.java
+++ src/main/java/lk/sajith/dep8/polling/dao/CrudDAO.java
@@ -3,6 +3,19 @@ package lk.sajith.dep8.polling.dao;
 import lk.sajith.dep8.polling.entity.SuperEntity;
 
 import java.io.Serializable;
+import java.util.List;
+import java.util.Optional;
 
 public interface CrudDAO<T extends SuperEntity, ID extends Serializable> extends SuperDAO {
+    T save(T entity);
+
+    void deleteById(ID pk);
+
+    Optional<T> findById(ID pk);
+
+    List<T> findAll();
+
+    long count();
+
+    boolean existsById(ID pk);
 }
--- src/main/java/lk/sajith/dep8/polling/dao/CrudDAOImpl.java
+++ src/main/java/lk/sajith/dep8/polling/dao/CrudDAOImpl.java
@@ -2,7 +2,53 @@ package lk.sajith.dep8.polling.dao;
 
 import lk.sajith.dep8.polling.entity.SuperEntity;
 
+import javax.persistence.EntityManager;
 import java.io.Serializable;
+import java.lang.reflect.ParameterizedType;
+import java.util.List;
+import java.util.Optional;
 
-public class CrudDAOImpl<T extends SuperEntity, ID extends Serializable> implements CrudDAO<T, ID> {
+public abstract class CrudDAOImpl<T extends SuperEntity, ID extends Serializable> implements CrudDAO<T, ID> {
+
+
+    protected EntityManager em;
+
+    private Class<T> entityClzObj;
+
+    public CrudDAOImpl() {
+        entityClzObj = (Class<T>)(((ParameterizedType)(this.getClass().getGenericSuperclass())).getActualTypeArguments()[0]);
+    }
+
+    @Override
+    public T save(T entity) {
+        em.persist(entity);
+        return entity;
+    }
+
+    @Override
+    public void deleteById(ID pk) {
+        em.remove(em.getReference(entityClzObj, pk));
+    }
+
+    @Override
+    public Optional<T> findById(ID pk) {
+        T entity = em.find(entityClzObj, pk);
+        return entity == null ? Optional.empty() : Optional.of(entity);
+    }
+
+    @Override
+    public List<T> findAll() {
+        return em.createQuery("SELECT e FROM " + entityClzObj.getName() + " e", entityClzObj).getResultList();
+    }
+
+    @Override
+    public long count() {
+        return em.createQuery("SELECT COUNT(e) FROM " + entityClzObj.getName() + " e", Long.class).getSingleResult();
+    }
+
+    @Override
+    public boolean existsById(ID pk) {
+        return findById(pk).isPresent();
+    }
+
 }
