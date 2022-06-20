c7ea73ae6fc096834572569a808acad8b3ff97cb
sajith <sajithd784@gmail.com>
2022-06-20 06:38:40 +0530

    listAllPolls in PollServiceImpl

--- src/main/java/lk/sajith/dep8/polling/service/custom/impl/PollServiceImpl.java
+++ src/main/java/lk/sajith/dep8/polling/service/custom/impl/PollServiceImpl.java
@@ -1,20 +1,28 @@
 package lk.sajith.dep8.polling.service.custom.impl;
 
+import lk.sajith.dep8.polling.dao.DAOFactory;
+import lk.sajith.dep8.polling.dao.custom.PollDAO;
 import lk.sajith.dep8.polling.dto.PollDTO;
 import lk.sajith.dep8.polling.service.custom.PollService;
+import lk.sajith.dep8.polling.service.util.EntityDTOTransformer;
 import lk.sajith.dep8.polling.util.JPAUtil;
 
 import javax.persistence.EntityManager;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PollServiceImpl implements PollService {
 
     @Override
     public List<PollDTO> listAllPolls() {
         EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
-
-
-        em.close();
+        PollDAO pollDAO = DAOFactory.getInstance().getDAO(em, DAOFactory.DAOType.POLL);
+        try {
+            return pollDAO.findAll().stream().map(EntityDTOTransformer::getPollDTO)
+                    .collect(Collectors.toList());
+        }finally{
+            em.close();
+        }
     }
 
     @Override
